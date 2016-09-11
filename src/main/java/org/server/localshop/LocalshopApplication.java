52eb9b5843b88797b746461e1eff2b8b957fd6bb
Tatali <Tatali>
2016-09-11 15:57:46 +0200

    code update

--- src/main/java/org/server/localshop/LocalshopApplication.java
+++ src/main/java/org/server/localshop/LocalshopApplication.java
@@ -3,9 +3,9 @@ package org.server.localshop;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-
 public class LocalshopApplication {
 
 	public static void main(String[] args) throws Exception
